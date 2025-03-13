f216fd94dca8c86f07bb8ecdbffc359cbd03f261
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-03-13 19:52:33 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Masthead from './components/Masthead.vue'
+</script>
+
+<template>
+  <header>
+    <Masthead />
+  </header>
+
+  <main>
+    O_o
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Masthead.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="masthead">
+    <div class="logo">
+      <h1>
+        Lucas Rodrigues
+      </h1>
+    </div>
+    <div class="menu">
+      <ul>
+        <li>
+          <a href="#Work">Work</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Links">Links</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.masthead {
+  width: 100%;
+  padding: 10px 20px;
+  display: absolute;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.menu ul {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 0px 10px;
+}
+</style>
